a7c0d12de0c503c2ec92d108189d937405bb08ed
Aranel9966 <99angelo.dire@gmail.com>
2023-05-04 18:01:43 +0200

    fix bug

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-// $dogSmall = new Animals('dog', 'small');
+
 $catSmall = new Articles('https://arcaplanet.vtexassets.com/arquivos/ids/259198/collare-gatto-majesty-rosso.jpg?v=1769174206', 'cat', 'small', 'collar', 'red', '3.30€');
 $catFood = new Food('https://m.media-amazon.com/images/I/81T80+an3WL._AC_SY550_.jpg', 'cat', 'small', 'croquettes', 'beef', '5.30€');
 $dogFood = new Food('https://m.media-amazon.com/images/I/81T80+an3WL._AC_SY550_.jpg', 'dog', 'big', 'croquettes', 'beef', '9.30€');
